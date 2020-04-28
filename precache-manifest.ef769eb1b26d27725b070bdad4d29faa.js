c6ff05d5374e9e4bb72aafb9ded2fb33e149b0a8
John Webster <JohnLobster@comcast.net>
2020-04-27 22:40:19 -0700

    Updates

---
+++ precache-manifest.ef769eb1b26d27725b070bdad4d29faa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f14092869246ef96f1bac203546ddc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f80a0b76435f94c4727",
+    "url": "/static/css/main.a41c8e6c.chunk.css"
+  },
+  {
+    "revision": "9d6d49373dfc098b0833",
+    "url": "/static/js/2.0871ec76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0871ec76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f80a0b76435f94c4727",
+    "url": "/static/js/main.e22fab23.chunk.js"
+  },
+  {
+    "revision": "612fd959eb7447039f8d",
+    "url": "/static/js/runtime-main.4bb7ede8.js"
+  },
+  {
+    "revision": "4f92f09402795b6ee999c2b25e4a5b33",
+    "url": "/static/media/FlatTP1.4f92f094.jpg"
+  },
+  {
+    "revision": "94ea8a4cd2dc321886ee0f4ab7a4352a",
+    "url": "/static/media/Lugosi.94ea8a4c.otf"
+  },
+  {
+    "revision": "cd5c8debcb9f1689abebea52d592ab70",
+    "url": "/static/media/cookie2.cd5c8deb.gif"
+  },
+  {
+    "revision": "2d42abe76f650b3170b66ef4162d8e28",
+    "url": "/static/media/covid_19_1.2d42abe7.png"
+  },
+  {
+    "revision": "ff76497e9c43c6d326ea731b2125f756",
+    "url": "/static/media/empty-toilet-paper.ff76497e.jpg"
+  },
+  {
+    "revision": "0d742d62c1c7d2b49a473e211674bfc1",
+    "url": "/static/media/hazard.0d742d62.png"
+  },
+  {
+    "revision": "f360e67b16f9ab5338f525b5752a491d",
+    "url": "/static/media/luxisb-webfont.f360e67b.woff"
+  },
+  {
+    "revision": "b477bcbaf892a5413c5388cc58bea9ec",
+    "url": "/static/media/paperBG.b477bcba.jpg"
+  },
+  {
+    "revision": "3cfa5e0621371fa5f3855eb3591d6fc1",
+    "url": "/static/media/tHandle.3cfa5e06.png"
+  },
+  {
+    "revision": "54260b4c7e7e15324eb8ca6adce0c9ef",
+    "url": "/static/media/tiles.54260b4c.jpg"
+  },
+  {
+    "revision": "a2ecf9923cfd9aa04e62909a55b1f397",
+    "url": "/static/media/tpCat.a2ecf992.gif"
+  },
+  {
+    "revision": "d990a6d7ded34628014506c3850cb5f4",
+    "url": "/static/media/tpTexture1.d990a6d7.jpg"
+  }
+]);
--- src/components/TpScroll/TpScroll.tsx
+++
@@ -1,9 +0,0 @@
-
-const TpScroll: (id:string) => void = (id) => {
-  const element = document.querySelector(id);
-  if (element) {
-    element.scrollIntoView()
-  }
-};
-
-export default TpScroll;
